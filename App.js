b635a84dd28be6736684f0adebd04d11b9c446b2
tamilmaranr <108916935+tamilmaranr@users.noreply.github.com>
2023-03-27 20:46:31 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+import Team from './Team'
+
+
+
+export class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <center>
+          <Team/>
+        </center>
+      </div>
+    )
+    
+  }
+}
+
+export default App
---
+++ Team.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Team() {
+  return (
+    <div>This is team functional component</div>
+  )
+}
+
+export default Team
